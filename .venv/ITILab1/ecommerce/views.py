63f79353f198da04c757e180e3e31936d1de4430
mido922 <mzohry@gmail.com>
2024-02-03 22:37:27 +0200

    Added forloop and context

--- .venv/ITILab1/ecommerce/views.py
+++ .venv/ITILab1/ecommerce/views.py
@@ -1,9 +1,57 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
-def helloCU(request):
-    return HttpResponse("Welcome to my ecommerce site.")
+
+def homePage(request):
+    template=loader.get_template('productList.html')
+    context = {
+    'products': [
+      {
+        'ID': '1',
+        'Name': 'Galaxy Phone',
+        'Description': 'A phone made by Samsung.',
+      },
+      {
+        'ID': '2',
+        'Name': 'SMP500 Laptop',
+        'Description': 'The laptop for investors on a budget.',
+      },
+      {
+        'ID': '3',
+        'Name': 'Galaxy Tab 7',
+        'Description': 'Samsung\'s newest iteration of the galaxy tab!',
+      }]
+    }
+    return HttpResponse(template.render(context, request))
+
+def category(request):
+    template=loader.get_template('productListCategories.html')
+    context = {
+    'products': [
+      {
+        'ID': '1',
+        'Name': 'Galaxy Phone',
+        'Description': 'A phone made by Samsung.',
+        'Category':'Phone',
+      },
+      {
+        'ID': '2',
+        'Name': 'SMP500 Laptop',
+        'Description': 'The laptop for investors on a budget.',
+        'Category':'Laptop'
+      },
+      {
+        'ID': '3',
+        'Name': 'Galaxy Tab 7',
+        'Description': 'Samsung\'s newest iteration of the galaxy tab!',
+        'Category':'Tablet',
+      }]
+    }
+    return HttpResponse(template.render(context, request))
+
 def aboutUs(request):
-    return HttpResponse("This website made for ITI in Django.")
+    template=loader.get_template('aboutUs.html')
+    return HttpResponse(template.render({}, request))
