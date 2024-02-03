c652666bffd47aec927a189d20ffd5a8a052dd4c
mido922 <mzohry@gmail.com>
2024-02-03 23:31:25 +0200

    Changed the way views are written.

--- .venv/ITILab1/ecommerce/urls.py
+++ .venv/ITILab1/ecommerce/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.homePage, name = 'homePage'),
     path('category/', views.category, name = 'category'),
-    path('aboutUs/', views.aboutUs, name= 'aboutUs')
+    path('aboutUs/', views.aboutUs, name = 'aboutUs'),
+    path('products/<int:productID>', views.productDetails, name = 'productDetails')
 ]
--- .venv/ITILab1/ecommerce/views.py
+++ .venv/ITILab1/ecommerce/views.py
@@ -4,54 +4,42 @@ from django.template import loader
 
 # Create your views here.
 
-
-def homePage(request):
-    template=loader.get_template('productList.html')
-    context = {
-    'products': [
+products=[
       {
-        'ID': '1',
-        'Name': 'Galaxy Phone',
-        'Description': 'A phone made by Samsung.',
-      },
-      {
-        'ID': '2',
-        'Name': 'SMP500 Laptop',
-        'Description': 'The laptop for investors on a budget.',
-      },
-      {
-        'ID': '3',
-        'Name': 'Galaxy Tab 7',
-        'Description': 'Samsung\'s newest iteration of the galaxy tab!',
-      }]
-    }
-    return HttpResponse(template.render(context, request))
-
-def category(request):
-    template=loader.get_template('productListCategories.html')
-    context = {
-    'products': [
-      {
-        'ID': '1',
+        'ID': 1,
         'Name': 'Galaxy Phone',
         'Description': 'A phone made by Samsung.',
         'Category':'Phone',
       },
       {
-        'ID': '2',
+        'ID': 2,
         'Name': 'SMP500 Laptop',
         'Description': 'The laptop for investors on a budget.',
         'Category':'Laptop'
       },
       {
-        'ID': '3',
+        'ID': 3,
         'Name': 'Galaxy Tab 7',
-        'Description': 'Samsung\'s newest iteration of the galaxy tab!',
+        'Description': 'Samsungs newest iteration of the galaxy tab!',
         'Category':'Tablet',
-      }]
-    }
-    return HttpResponse(template.render(context, request))
+      }
+]
+
+def homePage(request):
+    context = {}
+    context['products']=products
+    return render(request, 'productList.html', context)
+
+def category(request):
+    context = {}
+    context['products']=products
+    return render(request, 'productListCategories.html', context)
 
 def aboutUs(request):
-    template=loader.get_template('aboutUs.html')
-    return HttpResponse(template.render({}, request))
+    context = {}
+    return render(request, 'aboutUs.html', context)
+
+def productDetails(request, productID):
+    context = {}
+    context['products']=products
+    return render(request, 'aboutUs.html', context)
